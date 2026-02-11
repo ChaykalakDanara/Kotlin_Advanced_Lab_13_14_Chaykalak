de45d12d3d9016f582364bf26c60d51e3ca50456
mrultimatum07-alt <mrultimatum07@gmail.com>
2026-02-11 16:11:24 +0300

    Add extension properties and functions

--- src/Question.kt
+++ src/Question.kt
@@ -26,8 +26,6 @@ enum class Difficulty {
     HARD
 }
 
-
-
 class Quiz {
     val question1 = Question<String>(
         "Речка спятила с ума - По домам пошла сама.",
@@ -49,6 +47,17 @@ class Quiz {
         var answered: Int = 3
     }
 }
+val Quiz.StudentProgress.progressText: String
+    get() = "${answered} of ${total} answered"
+fun Quiz.StudentProgress.printProgressBar(){
+    repeat(Quiz.answered) { print("▓")}
+    repeat(Quiz.total - Quiz.answered) {print("print")}
+    println()
+    println(Quiz.progressText)
+}
+
 fun main(){
-    println("${Quiz.answered} of ${Quiz.total} answered")
+//    println("${Quiz.answered} of ${Quiz.total} answered")
+    println(Quiz.progressText)
+    Quiz.printProgressBar()
 }
