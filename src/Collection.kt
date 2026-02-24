60a1ac606c5a06972434b9ea07bb1ed9eb724b89
ChaykalakDanara <chaykalak.d08@gmail.com>
2026-02-25 00:00:13 +0300

    Lists in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -4,28 +4,54 @@ val solarSystem = rockPlanets + gasPlanets
 val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
 
 fun main() {
-    println(solarSystem[0])
-    println(solarSystem[1])
-    println(solarSystem[2])
-    println(solarSystem[3])
-    println(solarSystem[4])
-    println(solarSystem[5])
-    println(solarSystem[6])
-    println(solarSystem[7])
+    //Шаги 15.4-15.15.7
+//    val solarSystem = mutableListOf(
+//        "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"
+//    )
+//    solarSystem.add("Pluto")
+//    solarSystem.add( 3,  "Theia")
+//    solarSystem[3] = "Future Moon"
+//    println(solarSystem[3])
+//    println(solarSystem[9])
+//    solarSystem.removeAt(9)
+//    println(solarSystem.contains("Pluto"))
+//    println("Future Moon" in solarSystem)
 
-    solarSystem[3] = "Little Earth"
-    println(solarSystem[3])
-    println(newSolarSystem[8])
+    //Шаги 15.2–15.4
+//    val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+//    println(solarSystem.size)
+//    println(solarSystem[2])
+//    println(solarSystem.get(3))
+//    println(solarSystem.indexOf("Earth"))
+//    println(solarSystem.indexOf("Pluto"))
+//
+//    for (planet in solarSystem) {
+//        println(planet)
+//    }
 
-    val newSolarSystem = arrayOf(
-        "Mercury",
-        "Venus",
-        "Earth",
-        "Mars",
-        "Jupiter",
-        "Saturn",
-        "Uranus",
-        "Neptune",
-        "Pluto"
-    )
+    //Шаги 14
+//    println(solarSystem[0])
+//    println(solarSystem[1])
+//    println(solarSystem[2])
+//    println(solarSystem[3])
+//    println(solarSystem[4])
+//    println(solarSystem[5])
+//    println(solarSystem[6])
+//    println(solarSystem[7])
+//
+//    solarSystem[3] = "Little Earth"
+//    println(solarSystem[3])
+//    println(newSolarSystem[8])
+//
+//    val newSolarSystem = arrayOf(
+//        "Mercury",
+//        "Venus",
+//        "Earth",
+//        "Mars",
+//        "Jupiter",
+//        "Saturn",
+//        "Uranus",
+//        "Neptune",
+//        "Pluto"
+//    )
 }
