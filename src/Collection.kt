7644c1ada5e2ce521e23ea6ca70c32f39a09c358
ChaykalakDanara <chaykalak.d08@gmail.com>
2026-03-03 20:57:28 +0300

    forEach() function

--- src/Collection.kt
+++ src/Collection.kt
@@ -4,31 +4,41 @@ val solarSystem = rockPlanets + gasPlanets
 val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
 
 fun main() {
-    // Шаг 17
-//    val solarSystem = mutableMapOf(
-//        "Mercury" to 0,
-//        "Venus" to 0,
-//        "Earth" to 1,
-//        "Mars" to 2,
-//        "Jupiter" to 79,
-//        "Saturn" to 82,
-//        "Uranus" to 27,
-//        "Neptune" to 14
-//    )
-//    solarSystem["Pluto"] = 5
-//    println(solarSystem.size)
-//    println(solarSystem["Pluto"])
-//    println(solarSystem.get("Theia"))
-//    solarSystem.remove( "Pluto")
-//    solarSystem["Jupiter"] = 78
-//    println(solarSystem["Jupiter"])
+     //Шаг 17
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
+    println(solarSystem.size)
+    solarSystem["Pluto"] = 5
+    println(solarSystem.size)
+    println(solarSystem["Pluto"])
+    println(solarSystem.get("Theia"))
+
+    solarSystem.remove("Pluto")
+    println(solarSystem.size)
+    solarSystem["Jupiter"] = 78
+    println(solarSystem["Jupiter"])
     // Шаг 16
 //    val solarSystem = mutableSetOf(
 //        "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"
 //    )
+//    println(solarSystem.size)
 //    solarSystem.add("Pluto")
+
+//    println(solarSystem.size)
 //    println("Pluto" in solarSystem)
 //    solarSystem.remove("Pluto")
+
+//    println(solarSystem.size)
+//    solarSystem.remove(element = "Pluto")
+
 //    println(solarSystem.size)
 //    println(solarSystem.contains("Pluto"))
 
@@ -37,11 +47,12 @@ fun main() {
 //        "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"
 //    )
 //    solarSystem.add("Pluto")
-//    solarSystem.add( 3,  "Theia")
+//    solarSystem.add(3, "Theia")
 //    solarSystem[3] = "Future Moon"
 //    println(solarSystem[3])
 //    println(solarSystem[9])
 //    solarSystem.removeAt(9)
+//    solarSystem.remove("Future Moon")
 //    println(solarSystem.contains("Pluto"))
 //    println("Future Moon" in solarSystem)
 
