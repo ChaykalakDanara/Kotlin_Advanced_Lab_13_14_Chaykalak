d4b28fc38b546fa7a425a70005da3e7d067bde35
ChaykalakDanara <chaykalak.d08@gmail.com>
2026-03-03 21:13:46 +0300

    sortedBy() function

--- src/HigherOrderFunctions.kt
+++ src/HigherOrderFunctions.kt
@@ -83,4 +83,9 @@ fun main(){
     }
     println("Total price: $${totalPrice}")
 
+    val alphabeticalMenu = cookies.sortedBy { it.name }
+    println("Меню в алфавитном порядке:")
+    alphabeticalMenu.forEach {
+        println(it.name)
+    }
 }
